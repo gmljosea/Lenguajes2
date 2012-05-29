be68f4d527afc595359de891eeb95ee2f769d738
jenniferdr <jenniferdosreis@hotmail.com>
2012-05-28 22:05:03 -0430

    Print de Quad

---
+++ src/Quad.cc
@@ -0,0 +1,22 @@
+#include "Quad.hh"
+#include <iostream>
+
+void Quad::printQuad(){}
+
+void  AsignmentQ::printQuad(){
+  // Nombre del temporal
+  std::cout << this->result->getId() << ":= ";
+  if(arg1Type == id){
+    std::cout << this->arg1.id;
+  }else if(arg1Type ==constint){
+    std::cout << this->arg1.constint;
+  }else if(arg1Type == constfloat){
+    std::cout << this->arg1.constfloat;
+  }else if(arg1Type == constchar){
+    std::cout << this->arg1.constchar;
+  }else if(arg1Type == constbool){
+    std::cout << this->arg1.constbool;
+  }else if(arg1Type == conststring){
+    std::cout << this->arg1.conststring;
+  }
+}
