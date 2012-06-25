aa34d5ec8bd7398d4e48bacf852087f0e085ea78
jenniferdr <dosreisjennifer@hotmail.com>
2012-06-24 20:10:00 -0430

    instrucciones MIPS y agregue archivo genQuad

--- src/MIPSinstruction.hh
+++ src/MIPSinstruction.hh
@@ -33,32 +33,79 @@ enum Reg{
 
 };
 
+/* Load address La Rd,var */
+class La : public MIPSinstruction{
+private:
+  Reg Rd;
+  // La verdadera instruccion usa el nombre de la global
+  // Y si es local usar indireccion const(reg)
+  SymVar *addr;
+
+public:
+  Lw(Reg Rd,SymVar* addr): Rd(Rd),addr(addr){};
+};
+
+/* Load inmediate */
 class Li : MIPSinstruction{
+private:
+  Reg Rd;
+  int inmediate;
 
+public:
+  Li(Reg Rd,int inmed): Rd(Rd),inmediate(inmed){};
 };
 
-class Lw : MIPSinstruction{
+/* Load Word  Lw Rd,addr */
+class Lw : public MIPSinstruction{
+private:
+  Reg Rd;
+  // La verdadera instruccion usa el nombre de la global si lo es
+  // o el offset de la local y el $fp
+  SymVar *addr;
 
+public:
+  Lw(Reg Rd,SymVar* addr): Rd(Rd),addr(addr){};
 };
 
-class Sw : MIPSinstruction{
-
+// Store word Sw Rx,addr
+class Sw : public MIPSinstruction{
+private:
+  // Si es una local hay que tomar el offset mas el $fp
+  SymVar *addr;
+  Reg Rx;
+public:
+  Sw(SymVar* addr,Reg Rx): Rx(Rx),addr(addr){};
 };
 
-class Add : MIPSinstruction{
-
+class Add : public MIPSinstruction{
+private:
+  Reg Rx;
+  Reg Ry;
+  Reg Rz;
+public:
+  Add(Reg Rx,Reg Ry,Reg Rz): Rx(Rx),Ry(Ry),Rz(Rz){};
 };
 
-class Addi : MIPSinstruction{
-
+class Addi : public MIPSinstruction{
+private:
+  Reg Rx;
+  Reg Ry;
+  int inmediate;
+public:
+  Add(Reg Rx,Reg Ry,int inmed): Rx(Rx),Ry(Ry),inmediate(inmed){};
 };
 
 class AddS : MIPSinstruction{
 
 };
 
-class Sub : MIPSinstruction{
-
+class Sub : public MIPSinstruction{
+private:
+  Reg Rx;
+  Reg Ry;
+  Reg Rz;
+public:
+  Sub(Reg Rx,Reg Ry,Reg Rz): Rx(Rx),Ry(Ry),Rz(Rz){};
 };
 
 class Subi : MIPSinstruction{
@@ -69,8 +116,13 @@ class SubS : MIPSinstruction{
 
 };
 
-class Mul : MIPSinstruction{
-
+class Mul : public MIPSinstruction{
+private:
+  Reg Rx;
+  Reg Ry;
+  Reg Rz;
+public:
+  Mul(Reg Rx,Reg Ry,Reg Rz): Rx(Rx),Ry(Ry),Rz(Rz){};
 };
 
 class MulS : MIPSinstruction{
@@ -78,8 +130,26 @@ class MulS : MIPSinstruction{
 };
 
 // Deja el resultado en Lo y Hi
-class Div : MIPSinstruction{
+class Div : public MIPSinstruction{
+private:
+  Reg Rx;
+  Reg Ry;
+public:
+  Div(Reg Rx,Reg Ry): Rx(Rx),Ry(Ry){};
+};
 
+class Mfhi : public MIPSinstruction{
+private:
+  Reg reg;
+public:
+  Mfhi(Reg reg): reg(reg){};
+};
+
+class Mflo : public MIPSinstruction{
+private:
+  Reg reg;
+public:
+  Mflo(Reg reg): reg(reg){};
 };
 
 class DivS : MIPSinstruction{
@@ -112,33 +182,96 @@ class Not : MIPSinstruction{
 };
   
 // Branch less than  <
-class Blt : MIPSinstruction{
+class Blt : public MIPSinstruction{
+private:
+  Reg Rx;
+  Reg Ry;
+  std::string label;
 
+public:
+  Blt(Reg Rx,Reg Ry,std::string label): Rx(Rx),Ry(Ry),label(label){};
 };
 
 // Branch less Eq <=
-class Ble : MIPSinstruction{
+class Ble :public MIPSinstruction{
+private:
+  Reg Rx;
+  Reg Ry;
+  std::string label;
 
+public:
+  Ble(Reg Rx,Reg Ry,std::string label): Rx(Rx),Ry(Ry),label(label){};
 };
 
 // Branch Eq == 
-class Beq : MIPSinstruction{
+class Beq : public MIPSinstruction{
+private:
+  Reg Rx;
+  Reg Ry;
+  std::string label;
 
+public:
+  Beq(Reg Rx,Reg Ry,std::string label): Rx(Rx),Ry(Ry),label(label){};
 };
 
 // Branch Not Eq !=
-class Bne : MIPSinstruction{
+class Bne : public MIPSinstruction{
+private:
+  Reg Rx;
+  Reg Ry;
+  std::string label;
 
+public:
+  Bne(Reg Rx,Reg Ry,std::string label): Rx(Rx),Ry(Ry),label(label){};
 };
 
 // Branch Greather than >
-class Bgt : MIPSinstruction{
+class Bgt : public MIPSinstruction{
+private:
+  Reg Rx;
+  Reg Ry;
+  std::string label;
 
+public:
+  Bgt(Reg Rx,Reg Ry,std::string label): Rx(Rx),Ry(Ry),label(label){};
 };
 
 // Branch Greater Eq >=
-class Bgt : MIPSinstruction{
+class Bge : public MIPSinstruction{
+private:
+  Reg Rx;
+  Reg Ry;
+  std::string label;
 
+public:
+  Bge(Reg Rx,Reg Ry,std::string label): Rx(Rx),Ry(Ry),label(label){};
+};
+
+// Jump
+class J : public MIPSinstruction{
+private:
+  std::string label;
+
+public:
+  Bge(std::string label): label(label){};
+};
+
+// Jump and Link
+class Jal : public MIPSinstruction{
+private:
+  std::string label;
+
+public:
+  Jal(std::string label): label(label){};
+};
+
+// Jr 
+class Jr : public MIPSinstruction{
+private:
+  Reg Rx;
+
+public:
+  Jr(Reg Rx): Rx(Rx){};
 };
 
 #endif
---
+++ src/genQuad.hh
@@ -0,0 +1,45 @@
+// Metodos gen de los Quads 
+
+#inlude "Quad.hh"
+#inlude "MIPSinstruction.hh"
+
+/** 
+ * Asignacion de 3 o 2 direcciones
+ * x:= y op z  =>  Rd:= Rx op Ry  
+ * x:= op y    =>  Rd:= op Rx
+ **/
+List<MIPSinstrution*> AsignmentOpQ::gen(){
+  // Ver que registros convienen "getReg" para x, y 
+  // Si arg2 no en null ver que registro le conviene
+
+  // Si y(no es una constant) y no pertenece a Assig(Ry) 
+    // Buscar el y mas econom en Av(y)
+    // lista.add(new Lw(Ry,y)) 
+  // Si es una constante lista.add(new Li(Ry,y))
+
+  // Lo mismo para z en caso de no ser null
+  // Pero si usaremos addi y es const no hace falta colocarla en reg 
+  // lista.add(new Ld(Rz,z))
+
+  // Dependiendo del operador se gen la instruccion  
+  switch(this.op){
+  case Operator::sumI:
+    // Si el segundo es una constante usar Addi
+    //lista.add(new Add(Rx,Ry,Rz));
+  case Operator::sumF:
+    
+  case Operator::substractionI:
+    
+  case Operator::substractionF:
+    
+  case Operator::multiplicationI:
+    
+  case Operator::multiplicationF:
+    
+  case Operator::divisionI:
+  case Operator::divisionF:
+  case Operator::remainder:
+  case Operator::minusI:
+  case Operator::minusF:
+  }
+}
